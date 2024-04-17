864d85b96357eaba21eeffcd1e771b6b0ddc6c1c
azzaqihaq <azzaqihaq@gmail.com>
2024-04-17 10:20:40 +0700

    Change id with root

--- resources/js/product_defect_detection/index.jsx
+++ resources/js/product_defect_detection/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style/index.css";
 
-const root = createRoot(document.getElementById("app"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
