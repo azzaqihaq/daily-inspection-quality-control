db224ec6d0ae15d40237414801e77896803245d0
azzaqihaq <azzaqihaq@gmail.com>
2024-04-27 13:18:46 +0700

    Clean code yuhu

--- resources/js/product_defect_detection/components/btn-toggle-cam.jsx
+++ resources/js/product_defect_detection/components/btn-toggle-cam.jsx
@@ -5,26 +5,25 @@ const ButtonToggleCam = ({ cameraRef }) => {
   const [streaming, setStreaming] = useState(null); // streaming state
   const webcam = new Webcam(); // webcam handler
 
+  const handleToggleCam = () => {
+    if (streaming === null) {
+      webcam.open(cameraRef.current); // open webcam
+      cameraRef.current.style.display = "block"; // show camera
+      setStreaming("camera"); // set streaming to camera
+    } else if (streaming === "camera") {
+      webcam.close(cameraRef.current); // close webcam
+      cameraRef.current.style.display = "none"; // hide camera
+      setStreaming(null); // reset streaming state
+    } else {
+      alert(`Currently streaming: ${streaming}`); // if streaming another source
+    }
+  };
+
   return (
     <div className="btn-container">
       {/* Webcam Handler */}
-      <button
-        onClick={() => {
-          // if not streaming
-          if (streaming === null) {
-            webcam.open(cameraRef.current); // open webcam
-            cameraRef.current.style.display = "block"; // show camera
-            setStreaming("camera"); // set streaming to camera
-          }
-          // closing video streaming
-          else if (streaming === "camera") {
-            webcam.close(cameraRef.current);
-            cameraRef.current.style.display = "none";
-            setStreaming(null);
-          } else alert(`Currently streaming : ${streaming}`); // if streaming video
-        }}
-      >
-        {streaming === "camera" ? "Close" : "Open"} Webcam
+      <button onClick={handleToggleCam}>
+        {streaming === "camera" ? "Stop" : "Start"} Webcam
       </button>
     </div>
   );
