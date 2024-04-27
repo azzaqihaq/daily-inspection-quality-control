8b098d622bcbe2831f8031557fdb0f68186177ce
azzaqihaq <azzaqihaq@gmail.com>
2024-04-27 16:36:53 +0700

    Modify renderBox

--- resources/js/product_defect_detection/utils/renderBox.js
+++ resources/js/product_defect_detection/utils/renderBox.js
@@ -1,20 +1,12 @@
 import labels from "@trained_label/yolov8n_web_model.json";
 
-/**
- * Render prediction boxes
- * @param {HTMLCanvasElement} canvasRef canvas tag reference
- * @param {Array} boxes_data boxes array
- * @param {Array} scores_data scores array
- * @param {Array} classes_data class array
- * @param {Array[Number]} ratios boxes ratio [xRatio, yRatio]
- */
 export const renderBoxes = (canvasRef, boxes_data, scores_data, classes_data, ratios) => {
   const ctx = canvasRef.getContext("2d");
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clear canvas
 
   const colors = new Colors();
 
-  // font configs
+  // Font configuration
   const font = `${Math.max(
     Math.round(Math.max(ctx.canvas.width, ctx.canvas.height) / 40),
     14
@@ -36,16 +28,16 @@ export const renderBoxes = (canvasRef, boxes_data, scores_data, classes_data, ra
     const width = x2 - x1;
     const height = y2 - y1;
 
-    // draw box.
+    // Draw filled box with transparency
     ctx.fillStyle = Colors.hexToRgba(color, 0.2);
     ctx.fillRect(x1, y1, width, height);
 
-    // draw border box.
+    // Draw border box
     ctx.strokeStyle = color;
     ctx.lineWidth = Math.max(Math.min(ctx.canvas.width, ctx.canvas.height) / 200, 2.5);
     ctx.strokeRect(x1, y1, width, height);
 
-    // Draw the label background.
+    // Draw label background
     ctx.fillStyle = color;
     const textWidth = ctx.measureText(klass + " - " + score + "%").width;
     const textHeight = parseInt(font, 10); // base 10
@@ -64,34 +56,17 @@ export const renderBoxes = (canvasRef, boxes_data, scores_data, classes_data, ra
 };
 
 class Colors {
-  // ultralytics color palette https://ultralytics.com/
   constructor() {
     this.palette = [
-      "#FF3838",
-      "#FF9D97",
-      "#FF701F",
-      "#FFB21D",
-      "#CFD231",
-      "#48F90A",
-      "#92CC17",
-      "#3DDB86",
-      "#1A9334",
-      "#00D4BB",
-      "#2C99A8",
-      "#00C2FF",
-      "#344593",
-      "#6473FF",
-      "#0018EC",
-      "#8438FF",
-      "#520085",
-      "#CB38FF",
-      "#FF95C8",
-      "#FF37C7",
+      "#FF3838", "#FF9D97", "#FF701F", "#FFB21D", "#CFD231", "#48F90A",
+      "#92CC17", "#3DDB86", "#1A9334", "#00D4BB", "#2C99A8", "#00C2FF",
+      "#344593", "#6473FF", "#0018EC", "#8438FF", "#520085", "#CB38FF",
+      "#FF95C8", "#FF37C7"
     ];
-    this.n = this.palette.length;
+    this.paletteSize = this.palette.length;
   }
 
-  get = (i) => this.palette[Math.floor(i) % this.n];
+  get = (index) => this.palette[Math.floor(index) % this.paletteSize];
 
   static hexToRgba = (hex, alpha) => {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
