d3decefced3bf859448befead0986ce68de24ca5
azzaqihaq <azzaqihaq@gmail.com>
2024-05-15 22:22:33 +0700

    Modify color pallete

--- resources/js/part_scanner_indicator/utils/renderResult.js
+++ resources/js/part_scanner_indicator/utils/renderResult.js
@@ -67,10 +67,7 @@ export const renderResult = (canvasRef, boxesData, scoresData, classesData, rati
 class generateBoxColor {
   constructor() {
     this.palette = [
-      "#FF3838", "#FF9D97", "#FF701F", "#FFB21D", "#CFD231", "#48F90A",
-      "#92CC17", "#3DDB86", "#1A9334", "#00D4BB", "#2C99A8", "#00C2FF",
-      "#344593", "#6473FF", "#0018EC", "#8438FF", "#520085", "#CB38FF",
-      "#FF95C8", "#FF37C7"
+      "#FF3838", "#FF9D97"
     ];
     this.paletteSize = this.palette.length;
   }
--- resources/js/product_defect_detection/utils/renderFeedback.js
+++ resources/js/product_defect_detection/utils/renderFeedback.js
@@ -63,10 +63,7 @@ export const renderFeedback = (canvasRef, boxesData, scoresData, classesData, ra
 class generateBoxColor {
   constructor() {
     this.palette = [
-      "#FF3838", "#FF9D97", "#FF701F", "#FFB21D", "#CFD231", "#48F90A",
-      "#92CC17", "#3DDB86", "#1A9334", "#00D4BB", "#2C99A8", "#00C2FF",
-      "#344593", "#6473FF", "#0018EC", "#8438FF", "#520085", "#CB38FF",
-      "#FF95C8", "#FF37C7"
+      "#fc0000", "#00c0f0", "#ff6900", "#f5f000"
     ];
     this.paletteSize = this.palette.length;
   }
