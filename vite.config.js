1b54dff881d619c3fec4799ebaa9aaf865ba1bc3
azzaqihaq <azzaqihaq@gmail.com>
2024-05-16 23:42:02 +0700

    update vite path

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,12 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/app.js',
+                'resources/js/product_defect_detection/index.jsx',
+                'resources/js/part_scanner_indicator/index.jsx'
+            ],
             refresh: true,
         }),
         react(),
